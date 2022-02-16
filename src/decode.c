a3918f030a1a1949a686dc27ca2442afa8d5048d
bbbbbr <bbbbbr@users.noreply.github.com>
2022-02-16 14:00:30 -0800

    Decode Optimizations by @arpruss at ae17d83c3c19e4d5ab70d2eaf476044defb94bbe

--- src/decode.c
+++ src/decode.c
@@ -3,46 +3,51 @@
 #include <stdio.h>
 #endif
 
-//#pragma bank 3
 #include "encoded.h"
 
 uint8_t decodeInt(const uint8_t* blob, uint32_t* valueP) {
-    if (blob[0] & 0x80 ) {
-        *valueP = blob[0] & 0x7F;
+    uint8_t b = *blob;
+    *valueP = b & 0x7F;
+    if (b & 0x80) {
         return 1;
     }
-    else if (blob[1] & 0x80) {
-        *valueP = blob[0] | ((uint32_t)(blob[1]&0x7F)<<7);
-        return 2;
+    else {
+        b = *++blob;
+        *valueP |= (uint32_t)(b & 0x7F) << 7;
+        if (b & 0x80) {
+            return 2;
+        }
+        else {
+            *valueP |= (uint32_t)(blob[1] & 0x7F) << 14;
+            return 3;
+        }
     }
-    else /* if (blob[2] & 0x80) */ {
-        *valueP = blob[0] | ((uint32_t)(blob[1])<<7) | ((uint32_t)(blob[2]&0x7F)<<14);
-        return 3;
-    }    
 }
 
 void decodeWord(uint8_t start, uint32_t nextFour, char* buffer) {
-    buffer[0] = start + 'A';
-    buffer[1] = ((uint8_t)(nextFour >> 15) & 0x1F) + 'A';
-    buffer[2] = ((uint8_t)(nextFour >> 10) & 0x1F) + 'A';
-    buffer[3] = ((uint8_t)(nextFour >> 5) & 0x1F) + 'A';
-    buffer[4] = ((uint8_t)(nextFour) & 0x1F) + 'A';
-    buffer[5] = 0;
+    *buffer = start + 'A';
+    buffer += 5;
+    *buffer-- = 0;
+    for(uint8_t i=1;i<5;i++) {
+        *buffer-- = (nextFour & 0x1F) + 'A';
+        nextFour >>= 5;
+    }
 }
 
 void getWord(uint16_t n, char* buffer) {
     uint16_t count = 0;
     uint8_t i;
-    for (i = 0 ; i < 26 && n >= words[i+1].wordNumber ; i++) ;
+    const LetterList_t* w;
+    w = words;
+    for (i = 0 ; i < 26 && n >= w[1].wordNumber ; i++, w++) ;
     if (i == 26) {
         *buffer = 0;
         return;
     }
-    n -= words[i].wordNumber;
+    n -= w->wordNumber;
     uint32_t word = 0;
-    const uint8_t* blob = wordBlob + words[i].blobOffset;
-    uint16_t j;
-    for (j=0; j<=n; j++) {
+    const uint8_t* blob = wordBlob + w->blobOffset;
+    for (uint16_t j=0; j<=n; j++) {
         uint32_t delta;
         blob += decodeInt(blob, &delta);
         word += delta + 1;
@@ -51,11 +56,15 @@ void getWord(uint16_t n, char* buffer) {
 }
 
 uint8_t filterWord(char* s) {
-    for (int i=0; i<5; i++)
+    uint8_t i;
+    for (i=0; i<5; i++)
         if (s[i] < 'A' || s[i] > 'Z')
             return 0;
-    uint8_t i = s[0]-'A';
-    uint32_t w = ((uint32_t)(s[1]-'A') << 15) | ((uint32_t)(s[2]-'A') << 10) | ((uint32_t)(s[3]-'A') << 5) | (uint32_t)(s[4]-'A');
+    uint32_t w = 0;
+    for (uint8_t i=1;i<5;i++)
+        w = (w << 5) | (s[i]-'A');
+
+    i = s[0]-'A';
     uint16_t n = words[i+1].wordNumber - words[i].wordNumber;
     uint32_t match = 0;
     const uint8_t* b = wordBlob + words[i].blobOffset;
