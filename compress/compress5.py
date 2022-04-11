26a8e743239c95cb3fedf5a97afae04d2e19b623
bbbbbr <bbbbbr@users.noreply.github.com>
2022-04-11 01:15:10 -0700

    Reorganize a little - output for what should be existing version no longer matches - needs fixy

--- compress/compress5.py
+++ compress/compress5.py
@@ -13,9 +13,13 @@ NUM_ANSWER_BUCKETS = 15
 #       - could calc per dictionary, skipping first letters of words
 #       - english: https://en.wikipedia.org/wiki/Letter_frequency
 #         alpha_lookup = "esiarnmtodlcugpkhbyfvwjzxq"  # e = most frequent, q = least
+#       - http://www.norvig.com/mayzner.html
 # TODO:
 #   * 7 bit -> allicate unused bits for most common 2 or 3 letter sequences
 #       -  (calc per dict)
+#       -  Two-Letter Sequence (Bigram) Counts
+#           http://www.norvig.com/mayzner.html
+#   * Use word bit encoding "prefixes" (see notes)
 
 
 
@@ -37,14 +41,17 @@ NUM_ANSWER_BUCKETS = 15
 #  * Freq-2: Assign highest frequency letters the lowest value/first place in alphabet "aeioustryhkbcdfgjlmnpqvwxz"
 #            - hackernews
 
-
+# Letter Wordval W-Ord Alpha
 # 5-bit  7-bit                :  17,757
 # 5-bit  3-bit                :  16,315
 # B-26   3-bit                :  15,631
-# B-26   7-bit  W-Rev         :  14,856.5     <- has bitmap 1 bucket overflow (TODO)
+# B-26   7-bit  W-Rev         :  14,856.5  *  <- has bitmap 1 bucket overflow (TODO) <-- ERROR?
 # B-26   3-bit  W-Rev         :  14,304       <- has bitmap 1 bucket overflow (TODO)
 # B-26   3-bit  W-Rev  Freq-1 :  13,632.0     <- has bitmap 1 bucket overflow (TODO)
 # B-26   3-bit  W-Rev  Freq-2 :  13,253.0     <- has bitmap 1 bucket overflow (TODO)
+
+# B-26   7-bit  W-Rev  Freq-2 :  16,248       <- has bitmap 1 bucket overflow (TODO)
+
 
 
 # Optional arg 1 is language type (defaults to "en" otherwise)
@@ -115,8 +122,8 @@ def dumpBlob(name, blob):
 
 def remapAlpha(source_word):
     # TEST: reorganize letters to assign lowest values to highest frequency letters
-    # remaped_alpha = "esiarnmtodlcugpkhbyfvwjzxq"
-    remaped_alpha = "aeioustryhkbcdfgjlmnpqvwxz"
+    # remaped_alpha = "esiarnmtodlcugpkhbyfvwjzxq" # v1
+    remaped_alpha = "aeioustryhkbcdfgjlmnpqvwxz" # v2 (hackernews)
     remapped_word = ""
 
     for letter in range(len(source_word)):
@@ -132,9 +139,22 @@ def remapAlpha(source_word):
 # Reversing letters before encoding might save additional couple hundred bytes
   # Have to reverse sort each word coming in (answers + dict)
     # Then re-sort the lists first
-def tobinary(w):
+def tobinary_5bit(w):
 
-    w = remapAlpha(w)
+    # Alphabet frequency remapping
+    # w = remapAlpha(w)
+
+    # 5 bit encoding per letter
+    print(w + "...")
+    s = 0
+    for i in range(len(w)):
+        s = (s << 5) + (ord(w[i])-ord('a'))
+    print(w + " = " + str(s))
+    return s
+
+def tobinary_b26(w):
+
+    # w = remapAlpha(w)
 
     print(w + "...")
     s = 0
@@ -142,16 +162,12 @@ def tobinary(w):
     for i, c in enumerate(w.encode('ascii')[::-1]):
         s += (c - ord('a'))*26**i
 
-    # 5 bit encoding per letter
-    #for i in range(len(w)):
-    #    s = (s << 5) + (ord(w[i])-ord('a'))
     print(w + " = " + str(s))
     return s
 
 
-# TEST: variable length encoding with 3 bits instead of 7
-# TODO: needs packing of nybbles into bytes
-def encodeDelta(num):
+# requires later packing of nybbles into bytes
+def encodeDelta_3Bit(num):
     assert num > 0
     num -= 1
     if num == 0:
@@ -171,16 +187,21 @@ def encodeDelta(num):
             part |= 0x8
         res.append(part)
     return bytes(res)
-    # assert d<0x80*0x80*0x80
-    # if d < 0x80:
-    #     return bytes((0x80|d,))
-    # elif d < 0x80*0x80:
-    #     return bytes((d & 0x7F, 0x80|(d>>7)))
-    # else:
-    #     return bytes((d & 0x7F, (d>>7) & 0x7F, (d>>14)))
+
+# 7-bit word numeric encoding
+def encodeDelta_7Bit(d):
+    assert d<0x80*0x80*0x80
+    if d < 0x80:
+        return bytes((0x80|d,))
+    elif d < 0x80*0x80:
+        return bytes((d & 0x7F, 0x80|(d>>7)))
+    else:
+        return bytes((d & 0x7F, (d>>7) & 0x7F, (d>>14)))
 
 def encodeList(ww):
-    bin = tuple( map(tobinary, ww) )
+    # bin = tuple( map(tobinary, ww) )
+    bin = tuple( map(tobinary_5bit, ww) )
+    # bin = tuple( map(tobinary_b26, ww) )
     # bin = tuple( sorted(map(tobinary, ww)) )
     print("--------------------------------------------------")
     prev = 0
@@ -192,8 +213,11 @@ def encodeList(ww):
 #    for i in range(len(bin)):
 #        print(bin[i])
     for i in range(len(bin)):
-        out += encodeDelta(bin[i]-prev)
+        # out += encodeDelta(bin[i]-prev)
+        out += encodeDelta_7Bit(bin[i]-prev)
+        # out += encodeDelta_3Bit(bin[i]-prev)
         prev = bin[i]
+
     print("#################################################")
     return out
 
@@ -207,7 +231,7 @@ with open("full_" + lang + ".txt") as f:
     for w in f:
         w = w.strip()
         # reverse order of letters
-        w = w[::-1]
+        # w = w[::-1]
         if len(w) == 5:
             w = preprocessWord(w)
             allWords.add(w)
@@ -216,7 +240,7 @@ with open("answers_" + lang + ".txt") as f:
     for w in f:
         w = w.strip()
         # reverse order of letters
-        w = w[::-1]
+        # w = w[::-1]
         if len(w) == 5:
             w = preprocessWord(w)
             allWords.add(w)
@@ -230,6 +254,14 @@ for w in allWords:
     buckets[ord(w[0])-ord('a')].append(w[1:])
 
 encoded = tuple(map(encodeList, buckets))
+
+# 7bit word value encoding
+print(sum(map(len, encoded)))
+# 3bit encoding
+# print( ( sum(map(len, encoded) )  / 2) )
+
+
+
 offsets = []
 offset = 0
 for e in encoded:
@@ -302,10 +334,6 @@ with open(output_path + "sizes.h", "w") as sizes:
     sizes.write("#define NUM_ANSWERS %u\n" % len(answerWords))
     sizes.write("#define NUM_ANSWER_BUCKETS %u\n" % NUM_ANSWER_BUCKETS)
     sizes.write("#define NUM_ANSWERS_ROUNDED_UP_POW2 %u" % mask(len(answerWords)))
-
-# print(sum(map(len, encoded)))
-print( ( sum(map(len, encoded) )  / 2) )
-
 
 #print(max(map(len, encoded)))
 
