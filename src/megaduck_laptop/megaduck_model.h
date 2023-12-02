1d2f6b0be7cd15a4dc4a1c08ee516ced11f7d000
bbbbbr <bbbbbr@users.noreply.github.com>
2023-12-02 01:40:55 -0800

    Megaduck Laptop: fix header guard name

--- src/megaduck_laptop/megaduck_model.h
+++ src/megaduck_laptop/megaduck_model.h
@@ -1,8 +1,8 @@
 #include <gbdk/platform.h>
 #include <stdint.h>
 
-#ifndef _MEGADUCK_MODEL_IO_H
-#define _MEGADUCK_MODEL_IO_H
+#ifndef _MEGADUCK_MODEL_H
+#define _MEGADUCK_MODEL_H
 
 #define MEGADUCK_HANDHELD_STANDARD 0u
 #define MEGADUCK_LAPTOP_SPANISH    1u
@@ -12,4 +12,4 @@ extern uint8_t megaduck_model;
 
 void megaduck_laptop_check_model_vram_on_startup(void);
 
-#endif // _MEGADUCK_MODEL_IO_H
+#endif // _MEGADUCK_MODEL_H
